eded80b5a0514d629adb3b9d50a983e1eb15c2b2
majd-elhasan <96142959+majd-elhasan@users.noreply.github.com>
2022-02-11 21:10:31 +0300

    version0,.1

---
+++ fibonacciApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+ namespace fibonacci 
+ {
+     class MainClass
+     {
+        static void Main(string[] args)
+        {
+            List<int> fibonacci = new List<int>{0,1};
+            int n =0;
+            float fiboTotal = 0;
+            float fiboAverage = 0;
+            try{
+                Console.WriteLine("lütfen Fibonacci serisinin derinliğini giriniz");
+                n = Convert.ToInt32(Console.ReadLine());
+                }
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+                }
+            while (n > fibonacci.Count)
+            {
+                fibonacci.Add(fibonacci[fibonacci.Count-1]+ fibonacci[fibonacci.Count-2]);
+            }
+            foreach (var item in fibonacci)
+            {
+                fiboTotal += item;    
+            }
+            fiboAverage = fiboTotal/fibonacci.Count;
+            Console.WriteLine("_______________ortalama____________________");
+            Console.WriteLine(fiboAverage);
+            
+        }
+     }
+ }
